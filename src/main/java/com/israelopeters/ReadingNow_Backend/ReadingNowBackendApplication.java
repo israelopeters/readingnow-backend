ba6f80dc52bf3fac984f4a35f9637061b57d6194
israelopeters <israelopeters@gmail.com>
2025-04-26 21:12:51 +0100

    Initial commit

---
+++ src/main/java/com/israelopeters/ReadingNow_Backend/ReadingNowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.israelopeters.ReadingNow_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadingNowBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadingNowBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/israelopeters/ReadingNow_Backend/ReadingNowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.israelopeters.ReadingNow_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadingNowBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
